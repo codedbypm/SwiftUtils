91224b78fc9cdbc3907cf0a1fa942f6d6d8a056e
Paolo Moroni <paolo@codedby.pm>
2019-09-18 13:44:20 +0200

    Add APIRequestFactory

---
+++ Sources/SwiftUtils/APIRequestFactory.swift
@@ -0,0 +1,19 @@
+//
+//  APIRequestFactory.swift
+//  
+//
+//  Created by Paolo Moroni on 18/09/2019.
+//
+
+import Foundation
+
+protocol APIRequestFactory {
+    associatedtype T
+
+    func request(forRequestType type: T) -> URLRequest
+    func url(forRequestType type: T) -> URL
+    func query(forRequestType type: T) -> String
+    func headers(forRequestType type: T) -> [String: String]
+    func httpMethod(forRequestType type: T) -> String
+    func httpBody(forRequestType type: T) -> Data?
+}
