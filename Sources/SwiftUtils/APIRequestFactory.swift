1a207acb9197b7b533ec446193bbd6822e87b401
Paolo Moroni <paolo@codedby.pm>
2019-09-18 14:05:13 +0200

    Add default implementation of url(forRequestType) and request(ofType)

--- Sources/SwiftUtils/APIRequestFactory.swift
+++ Sources/SwiftUtils/APIRequestFactory.swift
@@ -10,10 +10,39 @@ import Foundation
 public protocol APIRequestFactory {
     associatedtype T
 
-    func request(forRequestType type: T) -> URLRequest
+    var host: String { get }
+    var scheme: String { get }
+
+    func request(ofType type: T) -> URLRequest
     func url(forRequestType type: T) -> URL
+    func path(forRequestType type: T) -> String
     func query(forRequestType type: T) -> String
     func headers(forRequestType type: T) -> [String: String]
     func httpMethod(forRequestType type: T) -> String
     func httpBody(forRequestType type: T) -> Data?
+}
+
+public extension APIRequestFactory {
+
+    func request(ofType type: T) -> URLRequest {
+        let requestURL = url(forRequestType: type)
+
+        let request = NSMutableURLRequest(url: requestURL)
+        request.allHTTPHeaderFields = headers(forRequestType: type)
+        request.httpMethod = httpMethod(forRequestType: type)
+        request.httpBody = httpBody(forRequestType: type)
+
+        return request as URLRequest
+    }
+
+    func url(forRequestType type: T) throws -> URL {
+
+        var components = URLComponents()
+        components.scheme = scheme
+        components.host = host
+        components.path = path(forRequestType: type)
+        components.query = query(forRequestType: type)
+
+        return components.url!
+    }
 }
