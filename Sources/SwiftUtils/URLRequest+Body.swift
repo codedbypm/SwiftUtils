251625f0496aebed5312d18c7a51133ad5b0870c
Paolo Moroni <paolo@codedby.pm>
2019-09-23 12:44:43 +0200

    Add httpBodyDictionary on URLRequest

---
+++ Sources/SwiftUtils/URLRequest+Body.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  
+//
+//  Created by Paolo Moroni on 23/09/2019.
+//
+
+import Foundation
+
+extension URLRequest {
+
+    var httpBodyDictionary: [String: String] {
+        guard let httpBody = httpBody else {
+            return [:]
+        }
+
+        let decodedHTTPBody = try? JSONSerialization.jsonObject(
+            with: httpBody,
+            options: []
+        )
+
+        guard let body = decodedHTTPBody as? [String: String] else {
+            return [:]
+        }
+
+        return body
+    }
+}
