520fc2926d825fa6e154ae35cfd059cec0563841
Paolo Moroni <paolo@codedby.pm>
2019-09-27 06:29:57 +0200

    Add httpBody to Dictionary<String, String>

---
+++ Sources/SwiftUtils/Dictionary+URLUtils.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by Paolo Moroni on 16/09/2019.
+//
+
+import Foundation
+
+public extension Dictionary where Key: StringProtocol, Value: StringProtocol {
+
+    var httpQuery: String {
+        return
+            map { return "\($0.key)=\($0.value)" }
+            .joined(separator: "&")
+    }
+
+    var httpBody: Data? {
+        return try? JSONSerialization.data(withJSONObject: self, options: [])
+    }
+}
