e96313eec06d77ca05e03f3f8783c0dafe4d3f95
Paolo Moroni <paolo@codedby.pm>
2019-10-15 15:01:30 +0200

    Add documentation

--- Sources/SwiftUtils/SwiftUI/Image.swift
+++ Sources/SwiftUtils/SwiftUI/Image.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 extension Image {
 
+    /// Create an `Image` from a `Data` object
+    /// - Parameter data: The data object containing the image data.
     public init?(data: Data) {
         guard let uiImage = UIImage(data: data) else { return nil }
         self = Image(uiImage: uiImage)
