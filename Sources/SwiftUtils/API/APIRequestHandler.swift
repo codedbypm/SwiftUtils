8763fde3d7f4c4985207e91b1b8b8f544177c309
Paolo Moroni <paolo@codedby.pm>
2021-01-04 13:03:25 +0100

    refactor: convert APIRequestHandler to use Combine

--- Sources/SwiftUtils/API/APIRequestHandler.swift
+++ Sources/SwiftUtils/API/APIRequestHandler.swift
@@ -5,58 +5,25 @@
 //  Created by Paolo Moroni on 10/11/2020.
 //
 
+import Combine
 import Foundation
-
-public enum APIRequestError: Error {
-    case noData
-    case decodingFailed(Error)
-}
 
 public protocol APIRequestHandler {
     var session: URLSession { get }
-    
-    func sendRequest<DTO: Decodable>(
-        _ request: URLRequest,
-        whenDone: @escaping OnResult<DTO>
-    )
+
+    func apiRequestPublisher<DTO: Decodable>(_ : URLRequest) -> AnyPublisher<DTO, Error>
 }
 
 public extension APIRequestHandler {
 
-    func sendRequest<DTO: Decodable>(
-        _ request: URLRequest,
-        whenDone: @escaping OnResult<DTO>
-    ) {
-        let task = session.dataTask(with: request) { (data, response, error) in
-            if let error = error {
-                whenDone(.failure(self.handleFailure(error)))
-            } else if let data = data {
-                whenDone(self.handleData(data))
-            } else {
-                whenDone(.failure(APIRequestError.noData))
-            }
-        }
-        task.resume()
-    }
-
-    private func handleFailure(_ error: Error) -> Error {
-        return error
-    }
-
-    private func handleData<DTO:Decodable>(_ data: Data) -> Result<DTO, Error> {
-        print("JSON string: \(String(data: data, encoding: .utf8) as Any)")
-
-        do {
-            if data.isEmpty, let emptyData = Empty.data {
-                let dto = try JSONDecoder().decode(DTO.self, from: emptyData)
-                return .success(dto)
-            } else {
-                let dto = try JSONDecoder().decode(DTO.self, from: data)
-                return .success(dto)
-            }
-        }
-        catch let error {
-            return .failure(APIRequestError.decodingFailed(error))
-        }
+    func apiRequestPublisher<DTO: Decodable>(
+        _ request: URLRequest
+    ) -> AnyPublisher<DTO, Error> {
+        return session
+            .dataTaskPublisher(for: request)
+            .map { $0.data }
+            .compactMap { $0.isEmpty ? Empty.data : $0 }
+            .decode(type: DTO.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
     }
 }
