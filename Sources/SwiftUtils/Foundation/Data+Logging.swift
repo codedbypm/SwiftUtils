8b1c189ce2d53078c56f06618d298999faaa72e9
Paolo Moroni <paolo@codedby.pm>
2021-01-24 18:29:51 +0100

    feat: add methods for pretty printing

---
+++ Sources/SwiftUtils/Foundation/Data+Logging.swift
@@ -0,0 +1,24 @@
+// 
+// Project: SwiftUtils
+// Copyright © 2021 codedby.pm. All rights reserved.
+//
+
+import Foundation
+
+public extension Data {
+
+    /// A pretty-printed version of the JSON data represented by the receiver
+    var prettyJSON: String? {
+        do {
+            let jsonObject = try JSONSerialization.jsonObject(with: self, options: [])
+            let data = try JSONSerialization.data(
+                withJSONObject: jsonObject,
+                options: [.sortedKeys, .prettyPrinted]
+            )
+            return String(data: data, encoding:.utf8)
+        }
+        catch {
+            return nil
+        }
+    }
+}
--- Sources/SwiftUtils/Foundation/Dictionary+URL.swift
+++ Sources/SwiftUtils/Foundation/Dictionary+URL.swift
@@ -9,6 +9,7 @@ import Foundation
 
 public extension Dictionary where Key: StringProtocol, Value: StringProtocol {
 
+    /// Returns a `String` representing the receiver as an HTTP query
     var httpQuery: String {
         return
             map { return "\($0.key)=\($0.value)" }
@@ -17,7 +18,19 @@ public extension Dictionary where Key: StringProtocol, Value: StringProtocol {
 }
 
 public extension Dictionary where Key: StringProtocol, Value == Any {
+
+    /// The `Data` representation of self when serialized in JSON format
     var httpBody: Data? {
         return try? JSONSerialization.data(withJSONObject: self, options: [])
+    }
+
+    /// A pretty-printed version of self
+    var prettyJSON: String? {
+        return try? JSONSerialization
+            .data(
+                withJSONObject: self,
+                options: [.sortedKeys, .prettyPrinted]
+            )
+            .prettyJSON
     }
 }
