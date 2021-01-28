efe73abeb35e3107374a53805c8c29a6d2e6a29e
Paolo Moroni <paolo@codedby.pm>
2021-01-28 10:24:02 +0100

    refactor: rename methods for CGPoint extension

--- Sources/SwiftUtils/Core Graphics/CGPoint.swift
+++ Sources/SwiftUtils/Core Graphics/CGPoint.swift
@@ -9,19 +9,19 @@ extension CGPoint {
 
     /// Returns a new  `CGPoint` whose `x` and `y` values equal the receiver `x` and `y`  values multiplied by -1
     /// - Returns: `CGPoint(x: -x, y: -y)`
-    func opposite() -> CGPoint {
-        return CGPoint(x: -x, y: -y)
+    var withOppositeCoordinates: CGPoint {
+        CGPoint(x: -x, y: -y)
     }
 
     /// Returns a new  `CGPoint` whose `x` value equals the receiver `x` value multiplied by -1
     /// - Returns: `CGPoint(x: -x, y: y)`
-    func oppositeX() -> CGPoint {
-        return CGPoint(x: -x, y: y)
+    var withOppositeX: CGPoint {
+        CGPoint(x: -x, y: y)
     }
 
     /// Returns a new  `CGPoint` whose `y` value equals the receiver `y` value multiplied by -1
     /// - Returns: `CGPoint(x: x, y: -y)`
-    func oppositeY() -> CGPoint {
-        return CGPoint(x: x, y: -y)
+    var withOppositeY: CGPoint {
+        CGPoint(x: x, y: -y)
     }
 }
